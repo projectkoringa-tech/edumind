cc08268aafd225a34bc7cf8e6b8180682096e232
projectkoringa-tech <projectkoringa@gmail.com>
2026-04-30 20:59:03 +0100

    Update TutorIA.tsx

--- src/pages/TutorIA.tsx
+++ src/pages/TutorIA.tsx
@@ -16,7 +16,7 @@ import { User } from 'firebase/auth';
 import { doc, addDoc, collection, serverTimestamp, setDoc, deleteDoc } from 'firebase/firestore';
 import { db, handleFirestoreError, OperationType } from '../firebase';
 import { StudySession, Message, UserProfile, Subject } from '../types';
-import { askTutor, generateLesson, generateExercises, assignHomework, FileData } from '../services/geminiService';
+import { askChatGPT, generateLesson, generateExercises, assignHomework, FileData } from '../services/openai'';
 import { cn } from '../lib/utils';
 import Markdown from 'react-markdown';
 import remarkMath from 'remark-math';
